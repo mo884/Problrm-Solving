58c633d5df64764dfad874136a1d23ceb4329fc1
Mohammed Essam Salem <77510501+mo884@users.noreply.github.com>
2023-12-31 20:48:02 +0200

    Add files via upload

---
+++ Plindrome_Number/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Plindrome_Number
+{
+    internal class Program
+    {
+
+
+        /// <summary>
+        /// Best Solution 
+        /// </summary>
+        /// 
+
+
+        static bool IsPalindrome(int x)
+        {
+            int number = x;
+            int reverseNumber =0;
+            
+            while(number !=0)
+            {
+                reverseNumber = number %10 + reverseNumber *10;
+                number = number /10;
+            }
+            return reverseNumber==x;
+        }
+            //static bool IsPalindrome(int x)
+            //{
+
+
+            //    //string number = Convert.ToString(x);
+            //    //if (number[0]=='-')
+            //    //    return false;
+            //    //if (number.Length == 1)
+            //    //    return true;
+
+            //    //string reversenumber = "";
+            //    //for (int i = number.Length-1; i>=0; i--)
+            //    //{
+            //    //    reversenumber +=number[i];
+            //    //}
+            //    //if (string.Equals(number, reversenumber))
+            //    //    return true;
+            //    //return false;
+            //}
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPalindrome(121));
+
+        }
+    }
+}
