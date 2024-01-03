940eaacc0b6c403bb65959a8631d9fec7d85c489
Mohammed Essam Salem <77510501+mo884@users.noreply.github.com>
2024-01-03 20:25:24 +0200

    Add files via upload

---
+++ Length of Last Word/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Length_of_Last_Word
+{
+    internal class Program
+    {
+        static int LengthOfLastWord(string s)
+        {
+            int count = 0;
+            for (int i = s.Length-1; i>=0; i--)
+            {
+                if (s[i] != ' ')
+                    count++;
+                else
+                    if (count>0)
+                    return count;
+            }
+            return count;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(LengthOfLastWord("   fly me   to   the moon  "));
+        }
+    }
+}
