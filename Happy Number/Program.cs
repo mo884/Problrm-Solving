518aece2764bf67930fff0c664248a1b187e87f2
Mohammed Essam Salem <77510501+mo884@users.noreply.github.com>
2024-01-03 20:12:30 +0200

    Add files via upload

---
+++ Happy Number/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Happy_Number
+{
+    internal class Program
+    {
+        static bool IsHappy(int n)
+        {
+            List<int> l = new List<int>();
+            int Number = 0;
+            while (n > 0)
+            {
+                int x;
+                Number = 0;
+                while (n > 0)
+                {
+                    x = n%10;
+                    n /= 10;
+                    Number += x*x;
+                }
+                n = Number;
+                if (Number ==1)
+                    return true;
+                if (!l.Contains(Number))
+                    l.Add(Number);
+                else
+                    return false;
+            }
+            return false;
+
+        }
+        static void Main(string[] args)
+        {
+             Console.WriteLine(IsHappy(2));
+        }
+    }
+}
