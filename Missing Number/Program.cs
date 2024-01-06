10f5de22ef65aa9d74db05777a23d4398b30864d
Mohammed Essam Salem <77510501+mo884@users.noreply.github.com>
2024-01-06 20:54:19 +0200

    Add files via upload

---
+++ Missing Number/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Missing_Number
+{
+    internal class Program
+    {
+        static int MissingNumber(int[] nums)
+        {
+            Array.Sort(nums);
+            if (nums[0]!=0)
+                return nums[0]-1;
+            for (int i = 0; i < nums.Length-1; i++)
+            {
+                if (nums[i]+1!=nums[i+1])
+                    if (i != nums.Length-1)
+                        return nums[i]+1;
+            }
+            return nums[nums.Length-1]+1;
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MissingNumber([0, 1]));
+        }
+    }
+}
